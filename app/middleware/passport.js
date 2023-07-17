1b50788ed53c2043b0df75150b0e93cd7789b81c
Pankaj Singh <pankaj.sandbox@gmail.com>
2023-07-17 16:41:31 +0530

    jwt fix

--- app/middleware/passport.js
+++ app/middleware/passport.js
@@ -1,36 +1,37 @@
-const {userServices} =require("../services")
-const  { Strategy:JwtStrategy,ExtractJwt } = require('passport-jwt');
+const { userServices } = require("../services");
+const { Strategy: JwtStrategy, ExtractJwt } = require("passport-jwt");
 
 const jwtOptions = {
-    secretOrKey: process.env.DB_SECRET,
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
-} 
+  secretOrKey: process.env.DB_SECRET,
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+};
 
-const jwtVerify = async(payload,done)=>{
-    console.log("jwt Strategy startted");
-    console.log("payload"+payload.sub);
-    try{
-        // const jwtTokenFromUser =ExtractJwt.fromAuthHeaderAsBearerToken();
-        //check whether the token is in database
-        //if not match deny say logout from others
-        //current ipaddress and device type check and match
+const jwtVerify = async (payload, done) => {
+  console.log("jwt Strategy startted");
+//   console.log("payload" + payload.sub);
+  try {
+    // const jwtTokenFromUser = ExtractJwt.fromAuthHeaderAsBearerToken();
+    //check whether the token is in database
+    //if not match deny say logout from others
+    //current ipaddress and device type check and match
 
-        const {user,token} = await userServices.getUserByUserId(payload.sub);
-        // if(token.token!==jwtTokenFromUser){
-        //     console.log("User token does not match,LogOut From Other device then Login again");
-        //     return done(null,false)
-        // }
-        if(!user){
-            return done(null,false)
-        }
-        done(null,user) 
-    } catch(error){
-        done(error,false)
+    const { user, token } = await userServices.getUserByUserId(payload.sub);
+    console.log("----------------------> Working", jwtTokenFromUser);
+    // if(token.token!==jwtTokenFromUser){
+    //     console.log("User token does not match,LogOut From Other device then Login again");
+    //     return done(null,false)
+    // }
+    if (!user) {
+      return done(null, false);
     }
-}
+    done(null, user);
+  } catch (error) {
+    done(error, false);
+  }
+};
 
-const jwtStrategy = new JwtStrategy(jwtOptions,jwtVerify);
+const jwtStrategy = new JwtStrategy(jwtOptions, jwtVerify);
 
 module.exports = {
-    jwtStrategy
-}
+  jwtStrategy,
+};
