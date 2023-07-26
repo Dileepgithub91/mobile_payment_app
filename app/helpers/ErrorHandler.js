7243204578c04084f55e964c45d97ba0c9965647
Kanhaiya Kumar Singh <138782449+kanhaiya-selectkaro@users.noreply.github.com>
2023-07-26 16:07:40 +0530

    updates
    updates

--- app/helpers/ErrorHandler.js
+++ app/helpers/ErrorHandler.js
@@ -3,6 +3,8 @@ const pinePerksErrorHandler=(error)=>{
     const status =error.status;
     console.log("error from handler");
     console.log(error);
+    console.log(error.code);
+    console.log(error.request);
     return error.message;
 }
 
