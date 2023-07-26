64f50ce607379103ddad82a18faa250731f338a4
Kanhaiya Kumar Singh <138782449+kanhaiya-selectkaro@users.noreply.github.com>
2023-07-26 15:26:09 +0530

    update
    update

--- app/helpers/ErrorHandler.js
+++ app/helpers/ErrorHandler.js
@@ -1,8 +1,8 @@
 const pinePerksErrorHandler=(error)=>{
-    const status =error.data.status;
+    const status =error.status;
     console.log("error from handler");
     console.log(error);
-    return error.data.message;
+    return error.message;
 }
 
 const qwikCilverErrorHandler=(error)=>{
