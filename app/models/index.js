1ea6a8d8878f0ed9882dc4e56e03de38cddf0436
Kanhaiya Kumar Singh <138782449+kanhaiya-selectkaro@users.noreply.github.com>
2023-07-28 17:25:12 +0530

    updates
    updates

--- app/models/index.js
+++ app/models/index.js
@@ -1,9 +1,8 @@
 const { Sequelize, DataTypes } = require("sequelize");
 const env = require("../env");
-// console.log("selectkaro_web_db", "root", "<[ZP5x,X", env("DB_HOST"));
-const sequelize = new Sequelize("selectkaro_web_db", "root", "<[ZP5x,X", {
-  host: "127.0.0.1",
-  dialect: "mysql"
+const sequelize = new Sequelize(env("DB_NAME"), env("DB_USER"), env("DB_PASS"), {
+  host: env("DB_HOST"),
+  dialect:"mysql"
 });
 // const config = require('config');
 // const dir = config.get('sqlite.logFileDir');
@@ -37,26 +36,13 @@ db.user_kyc_details = require("./user.kyc.details.model")(sequelize, DataTypes);
 db.user_profile = require("./user.profile.model")(sequelize, DataTypes);
 db.otp_verification = require("./otp.verification.model")(sequelize, DataTypes);
 db.user_token = require("./user.token.model")(sequelize, DataTypes);
-db.business_customer = require("./business.customer.model")(
-  sequelize,
-  DataTypes
-);
-db.business_agreement = require("./business.agreement.model")(
-  sequelize,
-  DataTypes
-);
-db.company_agreement_uploaded_document =
-  require("./company.agreement.upload.document.model")(sequelize, DataTypes);
+db.business_customer = require("./business.customer.model")(sequelize, DataTypes);
+db.business_agreement = require("./business.agreement.model")(sequelize, DataTypes);
+db.company_agreement_uploaded_document = require("./company.agreement.upload.document.model")(sequelize, DataTypes);
 db.kyc_pan_detail = require("./kyc.pan.response.model")(sequelize, DataTypes);
-db.kyc_aadhar_detail = require("./kyc.aadhar.response.model")(
-  sequelize,
-  DataTypes
-);
+db.kyc_aadhar_detail = require("./kyc.aadhar.response.model")(sequelize, DataTypes);
 db.kyc_gst_detail = require("./kyc.gst.response.model")(sequelize, DataTypes);
-db.api_provider_setting = require("./api.provider.setting.model")(
-  sequelize,
-  DataTypes
-);
+db.api_provider_setting = require("./api.provider.setting.model")(sequelize, DataTypes);
 // db.giftcard_category = require("./giftcard.category.model")(sequelize, DataTypes);
 // db.giftcard_products = require("./giftcard.product.model")(sequelize, DataTypes);
 // db.giftcard_orders = require("./giftcard.orders.model")(sequelize, DataTypes);
